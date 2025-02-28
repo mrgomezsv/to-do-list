ea52b2a81e5e1d7e49b205e3453347f720a933fe
Mario Roberto <mrgomez.dev@gmail.com>
2025-02-27 18:02:14 -0600

    First commit, task list completed

---
+++ script/script.js
@@ -0,0 +1,64 @@
+// Seleccionar elementos del DOM
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
+
+
+document.addEventListener('DOMContentLoaded', loadTasks); // Carga las tareas desde localStorage al iniciar
+
+addTaskBtn.addEventListener('click', () => { // Agregar tarea
+  const taskText = taskInput.value.trim();
+  if (taskText !== '') {
+    addTask(taskText);
+    taskInput.value = '';
+    saveTasks();
+  }
+});
+
+
+function addTask(taskText, isCompleted = false) {// Agrega una tarea a la lista
+  const li = document.createElement('li');
+  li.textContent = taskText;
+
+  if (isCompleted) {
+    li.classList.add('completed');
+  }
+
+
+  const deleteBtn = document.createElement('button');  // Botón para eliminar
+  deleteBtn.textContent = 'Eliminar';
+  deleteBtn.classList.add('deleteBtn');
+  deleteBtn.addEventListener('click', () => { // Evento del boton
+    taskList.removeChild(li);
+    saveTasks();
+  });
+
+
+  li.addEventListener('click', () => {  // Marco tarea como completada
+    li.classList.toggle('completed');
+    saveTasks();
+  });
+
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+}
+
+
+function saveTasks() {// Guardo las tareas en localStorage
+  const tasks = [];
+  taskList.querySelectorAll('li').forEach(li => {
+    tasks.push({
+      text: li.textContent.replace('Eliminar', '').trim(),
+      completed: li.classList.contains('completed')
+    });
+  });
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
+function loadTasks() { // Se cargan las tareas desde localStorage
+  const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+  tasks.forEach(task => {
+    addTask(task.text, task.completed);
+  });
+}
